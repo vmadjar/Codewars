11eb5079d220b193ce22f8d6536aef1dc5e07212
vmadjar <vasya.madzhar@gmail.com>
2022-02-10 12:06:32 +0200

    First task

---
+++ codewars.js
@@ -0,0 +1,18 @@
+//  Write a function that takes in a string of one or more words,
+// and returns the same string, but with all five or more letter words reversed
+// (Just like the name of this Kata).
+//  Strings passed in will consist of only letters and spaces.
+//   Spaces will be included only when more than one word is present.
+
+function spinWords(string) {
+  return string
+    .split(" ")
+    .map((str) => {
+      if (str.length >= 5) {
+        return str.split("").reverse().join("");
+      } else {
+        return str;
+      }
+    })
+    .join(" ");
+}
