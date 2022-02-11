f2ff31b385cd08c624ef509eddd889688aacf311
vmadjar <vasya.madzhar@gmail.com>
2022-02-11 18:12:15 +0200

    added second solved task

---
+++ codevars2.js
@@ -0,0 +1,14 @@
+function chooseBestSum(t, k, ls){
+    if (ls.length < k) return null
+    const results = [[]]
+    for (const value of ls) {
+        const copy = [...results]
+        for (const prefix of copy) {
+            results.push(prefix.concat(value))
+        }
+    }
+    return results
+        .filter((a => a.length && a.length == k ))
+        .map(item => item.reduce((a,b) => a + b), 0)
+        .filter(item => item <= t)
+        .sort((a,b) => b-a)[0] || null
